597060180acdba1272a6144d39471e3625dde144
mkerrigan <michael.kerrigan@gmail.com>
2019-09-15 13:07:57 -0500

    post-inital commit

---
+++ src/main/java/org/pengu/service/CakeService.java
@@ -0,0 +1,12 @@
+package org.pengu.service;
+
+import org.pengu.model.Cake;
+
+import java.util.List;
+
+public class CakeService {
+
+    public int maxDuffelBagValue(List<Cake> cakeList){
+        return -1;
+    }
+}
---
+++ src/test/java/org/pengu/service/CakeServiceTest.java
@@ -0,0 +1,10 @@
+package org.pengu.service;
+
+import org.junit.Test;
+
+public class CakeServiceTest {
+
+    @Test
+    public void maxDuffelBagValue() {
+    }
+}
