b86708d17c122bdfa428cbaaa9a3f7d08d1e7c12
fuzzy <jakedude18@gmail.com>
2019-09-15 18:10:20 -0500

    easy dubz

--- src/main/java/org/pengu/service/CakeService.java
+++ src/main/java/org/pengu/service/CakeService.java
@@ -2,14 +2,13 @@ package org.pengu.service;
 
 import org.pengu.model.Cake;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CakeService {
+ class CakeService {
     private LinkedList<Cake> sortedCakes;
-    public int maxDuffelBagValue(List<Cake> cakeList, int weight) {
+    int maxDuffelBagValue(List<Cake> cakeList, int weight) {
 
         //sort cakes by order
         class sortByValue implements Comparator<Cake> {
@@ -18,15 +17,15 @@ public class CakeService {
                 return a.getValue()/a.getWeight()-b.getValue()/b.getWeight();
             }
         }
-        sortedCakes = new LinkedList(cakeList);
-        Collections.sort(sortedCakes,new sortByValue());
+        sortedCakes = new LinkedList<>(cakeList);
+        sortedCakes.sort(new sortByValue());
         /*System.out.println("cakes sorted by value density");
         for(Cake cake : sortedCakes){
             System.out.println(cake);
         }*/
         return findMaxDuffelBagValue(weight);
     }
-    int findMaxDuffelBagValue(int weight){
+    private int findMaxDuffelBagValue(int weight){
         int extraSpace  = weight%sortedCakes.getLast().getWeight();
         int value = weight/sortedCakes.getLast().getWeight() * sortedCakes.getLast().getValue();
         sortedCakes.removeLast();
