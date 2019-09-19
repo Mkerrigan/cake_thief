75bf1768e59ca8fcacc06141de90406328e1e9ae
fuzzy <jakedude18@gmail.com>
2019-09-19 00:13:42 -0500

    cleaner and more efficient solution

--- src/main/java/org/pengu/service/CakeService.java
+++ src/main/java/org/pengu/service/CakeService.java
@@ -2,13 +2,13 @@ package org.pengu.service;
 
 import org.pengu.model.Cake;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 
  class CakeService {
+     private ArrayList<Cake> sortedCakes;
      int maxDuffelBagValue(List<Cake> cakeList, int weight) {
-         LinkedList<Cake> sortedCakes;
          //sort cakes by order
         class sortByValue implements Comparator<Cake> {
             @Override
@@ -16,32 +16,25 @@ import java.util.List;
                 return a.getValue() / a.getWeight() - b.getValue() / b.getWeight();
             }
         }
-        sortedCakes = new LinkedList<>(cakeList);
+        sortedCakes = new ArrayList<>(cakeList);
         sortedCakes.sort(new sortByValue());
         /*System.out.println("cakes sorted by value density");
         for(Cake cake : sortedCakes){
             System.out.println(cake);
         }*/
-        class duffelBag {
-            private LinkedList<Cake> sortedCakes;
-            private int weight;
-            private duffelBag(LinkedList<Cake> sortedCakes,int weight){
-                this.sortedCakes = sortedCakes;
-                this.weight = weight;
-            }
-            private int maxValue() {
-                Cake cake = sortedCakes.getLast();
-                int extraSpace = weight % cake.getWeight();
-                int value = weight / cake.getWeight() * cake.getValue();
-                sortedCakes.removeLast();
-                if(extraSpace != 0 && !sortedCakes.isEmpty()){
-                    int multipleCakesValue = new duffelBag(new LinkedList<>(sortedCakes),extraSpace + cake.getWeight()).maxValue();
-                    if(multipleCakesValue >= cake.getValue()) return value - cake.getValue() + multipleCakesValue;
-                }
-                return extraSpace == 0 || sortedCakes.isEmpty() ? value: value + new duffelBag(sortedCakes,extraSpace).maxValue();
-            }
-        }
-        return new duffelBag(sortedCakes,weight).maxValue();
+
+
+        return maxValue(weight,1);
+     }
+     private int maxValue(int space,int index) {
+         Cake cake = sortedCakes.get(sortedCakes.size()-index);
+         int extraSpace = space % cake.getWeight();
+         int value = space / cake.getWeight() * cake.getValue();
+         if(extraSpace != 0 && sortedCakes.size() - index != 0 && extraSpace != space){
+             int multipleCakesValue = maxValue(extraSpace + cake.getWeight(),index + 1);
+             if(multipleCakesValue >= cake.getValue()) return value - cake.getValue() + multipleCakesValue;
+         }
+         return extraSpace == 0 || sortedCakes.size() - index == 0 ? value: value + maxValue(extraSpace,index + 1);
      }
  }
 
